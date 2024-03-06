0e7ddda3b00a046b3d444978df598d5a435a6474
likhithkunder <112951110+likhithkunder@users.noreply.github.com>
2024-03-06 23:23:03 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "The webhook for my repository is now active\n";
+  
   return 0;
 }
